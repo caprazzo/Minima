762dcc514c6411e77ecc26eda6344830fcc3774b
Matteo Caprari <matteo.caprari@gmail.com>
2011-10-18 11:16:52 +0100

    enabled db autopurge

--- src/main/java/net/caprazzi/minima/MiniMain.java
+++ src/main/java/net/caprazzi/minima/MiniMain.java
@@ -1,6 +1,5 @@
 package net.caprazzi.minima;
 
-import net.caprazzi.keez.Keez;
 import net.caprazzi.keez.simpleFileDb.KeezFileDb;
 import net.caprazzi.minima.service.MinimaService;
 import net.caprazzi.minima.servlet.MinimaPushServlet;
@@ -8,7 +7,8 @@ import net.caprazzi.minima.servlet.MinimaPushServlet;
 public class MiniMain {
 
 	public static void main(String[] args) throws Exception {
-		Keez.Db db = new KeezFileDb("./db", "minimav0");
+		KeezFileDb db = new KeezFileDb("./db", "minimav0");
+		db.setAutoPurge(true);
 		MinimaPushServlet pushServlet = new MinimaPushServlet();	
 		MinimaService minimaService = new MinimaService(db, pushServlet);
 		MinimaServer minimaServer = new MinimaServer(minimaService, pushServlet);
