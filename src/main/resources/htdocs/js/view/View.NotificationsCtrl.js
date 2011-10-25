40bd67b2c24d7b0904e8d48f1a0df716657cd81e
Matteo Caprari <matteo.caprari@gmail.com>
2011-10-25 16:56:05 +0100

    notification control ui is in place

--- src/main/resources/htdocs/js/view/View.NotificationsCtrl.js
+++ src/main/resources/htdocs/js/view/View.NotificationsCtrl.js
@@ -18,8 +18,7 @@ window.NotificationsCtrlModel = Backbone.Model.extend({
 		}
 	},
 	
-	activate: function() {
-		
+	activate: function() {		
 		var that = this;
 		if (this.getPermission() == 'not_allowed') {
 			window.webkitNotifications.requestPermission(function() {
@@ -61,19 +60,34 @@ NotificationsCtrlView = Backbone.View.extend({
 	},
 	  	  
 	events: {
-		'click .notifications-ctrl-off': 'deactivate',
-		'click .notifications-ctrl-on': 'activate'		
+		'click #mn-tpl-notifications-ctrl-off': 'deactivate',
+		'click #mn-tpl-notifications-ctrl-on': 'activate'		
 	},
 
 	render: function() {
-		$(this.el).html(this.template(this.model.toJSON()));
+		var $el = $(this.el);
+		$el.html(this.template(this.model.toJSON()));
+		$el.find('.mn-tpl-notifications-ctrl-radio').buttonset();		
+		$el.find('.mn-tpl-notifications-ctrl-help').hover(
+			function() { $(this).addClass('mn-tpl-notifications-ctrl-help-hover'); },
+			function() { $(this).removeClass('mn-tpl-notifications-ctrl-help-hover'); }
+		).click(function() {
+			$el.find('.mn-tpl-notification-ctrl-help-topic').toggle();
+			$el.find('.mn-tpl-notifications-ctrl-help').toggleClass('mn-tpl-notifications-ctrl-help-active');
+		});
+		$topic = $el.find('.mn-tpl-notification-ctrl-help-topic');
+		var out = $topic.outerWidth();
+		var ins = $topic.width();
+		$topic.width($el.width() - (out - ins));
 	},
 	
 	activate: function() {
+		console.log('Notification Ctrl enable');
 		this.model.activate();
 	},
 	
 	deactivate: function() {
+		console.log('Notification Ctrl disable');
 		this.model.deactivate()
 	}
 });
